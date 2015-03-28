afce6d8a80a01d591bd7448a27e3803c0bb0bc30
Graham St-Laurent <gstlaurent@gmail.com>
2015-03-28 15:48:38 -0700

    Playing with the views.py

--- plants/waterplants/views.py
+++ plants/waterplants/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+from waterplants.models import Plant
 
 def index(request):
     template = loader.get_template('waterplants/index.html')
+
+    # allplants = Plant.object.all()
     myplants = [('Bob', 'Rose', 10), ('Jericho', 'Cactus', 99)] # (Name, Type, Number of seconds til Needs to be Watered)
     planttypes = ['Rose', 'Cactus', 'Venus Fly Trap'] # Plant type names
+
+
     context = RequestContext(request, { 'myplants': myplants, 'planttypes': planttypes} )
     return HttpResponse(template.render(context))
 
