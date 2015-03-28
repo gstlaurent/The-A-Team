a5fd41ef853d161fbb76aa70edbb769580bcc656
Shaylene Beaudry <shaybeau731@gmail.com>
2015-03-28 14:38:30 -0700

    adding migration file for userplants

--- plants/waterplants/views.py
+++ plants/waterplants/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-    
+    template = loader.get_template('waterplants/index.html')
+    context = RequestContext(request, { 'something': [1,2,3]})
+    return HttpResponse(template.render(context))
