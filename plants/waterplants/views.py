940114b913e3ae0d139627d126e9699cb5f47929
Graham St-Laurent <gstlaurent@gmail.com>
2015-03-28 16:09:54 -0700

    Added table of plants

--- plants/waterplants/views.py
+++ plants/waterplants/views.py
@@ -6,8 +6,8 @@ from waterplants.models import Plant
 def index(request):
     template = loader.get_template('waterplants/index.html')
 
-    # allplants = Plant.object.all()
-    myplants = [('Bob', 'Rose', 10), ('Jericho', 'Cactus', 99)] # (Name, Type, Number of seconds til Needs to be Watered)
+    myplants = Plant.objects.all()
+    # myplants = [('Bob', 'Rose', 10), ('Jericho', 'Cactus', 99)] # (Name, Type, Number of seconds til Needs to be Watered)
     planttypes = ['Rose', 'Cactus', 'Venus Fly Trap'] # Plant type names
 
 
