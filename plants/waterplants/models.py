c1e0d65c41770730a7d35d2bffdd77fa55485dc1
Graham St-Laurent <gstlaurent@gmail.com>
2015-03-28 16:29:54 -0700

    Got rid of commit errors in models.py

--- plants/waterplants/models.py
+++ plants/waterplants/models.py
@@ -4,11 +4,7 @@ from django.db import models
 
 class Plant(models.Model):
     type = models.CharField(max_length=100)
-<<<<<<< HEAD
-    interval = models.Integerfield()
-=======
     interval = models.IntegerField()
->>>>>>> 62d6d170541372ccf56ce8567b604ae377c967a8
 
 class UserPlants(models.Model):
     name = models.CharField(max_length=100)
