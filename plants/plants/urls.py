1f75b9c82680963b6e158938b90dd66114bc54cb
Graham St-Laurent <gstlaurent@gmail.com>
2015-03-28 13:36:32 -0700

    Added a waterplant endpoint

--- plants/plants/urls.py
+++ plants/plants/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'plants.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^waterplants/', include('waterplants.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
